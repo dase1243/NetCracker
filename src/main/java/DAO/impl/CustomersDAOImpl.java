5ad0bfabfd2f9d9c7750064f19f4c457668e4de6
dase <drei@glushkogmail.com>
2016-11-16 15:05:27 +0300

    first daoCustomers

--- src/main/java/DAO/impl/CustomersDAOImpl.java
+++ src/main/java/DAO/impl/CustomersDAOImpl.java
@@ -13,7 +13,8 @@ import java.util.List;
 import org.hibernate.Hibernate;
 import org.hibernate.Session;
 import org.hibernate.jmx.HibernateService;
-import util.HibernateUtil;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 
 /**
  * Created by dreikaa on 11/13/16.
@@ -21,28 +22,38 @@ import util.HibernateUtil;
 public class CustomersDAOImpl implements CustomersDAO {
     private Session session;
 
-    public CustomersDAOImpl(){
-        session =
-        //session = HibernateUtil.getSessionFactory().openSession();
+    public CustomersDAOImpl() {
+        session = HibernateUtil.getSessionFactory().openSession();
     }
 
     public void addCustomer(Customers customer) throws SQLException {
-
+        session.beginTransaction();
+        session.save(customer);
+        session.getTransaction().commit();
     }
 
     public void updateCustomer(Customers customer) throws SQLException {
-
+        session.beginTransaction();
+        session.update(customer);
+        session.getTransaction().commit();
     }
 
     public Customers getCustomerById(Long id) throws SQLException {
-        return null;
+        Customers customer = null;
+        customer = (Customers) session.load(Customers.class, id);
+        return customer;
     }
 
     public Collection<Customers> getAllCustomers() throws SQLException {
-        return null;
+        List<Customers> customer = new ArrayList<Customers>();
+        customer = session.createCriteria(Customers.class).list();
+        return customer;
     }
 
     public void deleteCustomer(Customers customer) throws SQLException {
-
+        session.beginTransaction();
+        session.delete(customer);
+        session.getTransaction().commit();
     }
 }
+
